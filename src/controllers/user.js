ec3307c3fd980520d15df789e4c0a6a1ea5df840
Ryan Levell <rlevell@KNY-Dell1036.XPX.intra>
2014-11-12 14:58:29 -0600

    Complete with initial code for user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,9 @@
+var qs = require('querystring');
+var url = require('url');
+var path = require('path');
+
 function user(){
-  console.log("constructed user controller");
+  console.log("CONSTRUCTED USER CONTROLLER");
 };
 
 function userObj(id, firstName, lastName, email, phone, office, password) {
@@ -12,51 +16,107 @@ function userObj(id, firstName, lastName, email, phone, office, password) {
   this.password = password;
 };
 
-function createUser(request, response) {
-  console.log("creating user");
+function createUser(request, response, fn) {
+	console.log("CREATING USER...");
   
-   // TODO: implement create
+	var body = '';
+	
+	request.on('data', function (data) {
+		body += data;
+	});
+	
+	request.on('end', function () {
+		var post = qs.parse(body);
+		
+		// dummy id
+		id = Math.floor(Math.random() * 10000);
+		var u = new userObj(id, post['firstName'], post['lastName'], post['email'], post['phone'], post['office'], post['password']);
+		
+		var json = JSON.stringify(u);
+		console.log("CREATED USER: " + json);
+		
+		response.write(json);
+		fn(response);
+	});
 };
 
-function readUser(request, response) {
-  console.log("reading user");
+function readUser(request, response, fn) {
+  console.log("READING USER...");
   
-  // TODO: get id from url
-  var id = 123;
+  var id = path.basename(url.parse(request.url).pathname);
   
   // dummy data
   var u = new userObj(id, 'Spongebob', 'Squarepants', 'bob@xpanxion.com', '555-555-5555', 123, 'squidward');
   var json = JSON.stringify(u);
-  console.log("read user json: " + json);
-  return json;
-};
-
-function updateUser(request, response) {
-  console.log("updating user");
+  console.log("READ USER: " + json);
   
-  // TODO: implement update
+  response.write(json);
+  fn(response);
 };
 
-function deleteUser(request, response) {
-  console.log("deleting user");
+function updateUser(request, response, fn) {
+	console.log("UPDATING USER");
   
-   // TODO: implement delete
+	var id = path.basename(url.parse(request.url).pathname);
+	var body = '';
+	
+	request.on('data', function (data) {
+		body += data;
+	});
+	
+	request.on('end', function () {
+		var post = qs.parse(body);
+		
+		// dummy data
+		var u = new userObj(id, 'Spongebob', 'Squarepants', 'bob@xpanxion.com', '555-555-5555', 123, 'squidward');
+		
+		// set fields to new values
+		for(var prop in post) {
+			if(typeof u[prop] !== 'undefined') {
+				u[prop] = post[prop];
+			}
+		}
+		
+		var json = JSON.stringify(u);
+		console.log("UPDATED USER: " + json);
+		
+		response.write(json);
+		fn(response);
+	});
 };
 
-user.prototype.processRequest = function(request, response) {
-  switch(request.method) {
-    case "POST":
-	  createUser(request, response);
-	  break;
-	case "GET":
-	  readUser(request, response);
-	  break;
-	case "PUT":
-	  updateUser(request, response);
-	  break;
-	case "DELETE":
-	  deleteUser(request, response);
-	  break;
+function deleteUser(request, response, fn) {
+	console.log("DELETING USER...");
+  
+	var id = path.basename(url.parse(request.url).pathname);
+	console.log('DELETED USER: ' + id);
+   
+	fn(response);
+};
+
+user.prototype.processRequest = function(request, response, fn) {
+
+	switch(request.method) {
+		case "POST":
+			createUser(request, response, function() {
+				fn(response);
+			});
+			break;
+		case "GET":
+			readUser(request, response, function() {
+				fn(response);
+			});
+			break;
+		case "PUT":
+			updateUser(request, response, function() {
+				fn(response);
+			});
+			break;
+		case "DELETE":
+			deleteUser(request, response, function() {
+				fn(response);
+			});
+		break;
 	}
 };
 
