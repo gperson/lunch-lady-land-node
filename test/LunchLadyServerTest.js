4158e9c252fe5ddbe016a2b306f4f2b6bb06cb98
Brian Smith <brian.darklance@gmail.com>
2015-03-09 22:40:19 -0500

    updates to tests

--- test/LunchLadyServerTest.js
+++ test/LunchLadyServerTest.js
@@ -8,20 +8,14 @@ describe("LunchLadyLand" , function (){
 			var response = new Object(); 
 			var connection = new Object(); 
 			var controller = { handleRequest: function(){}};
-			
-			
-			
-			unit.given("All inputs are valid and controller(s) are set", function (){
-				request.url = "http://www.test.com:1234/v1/test";
-				unit.spy(controller, "handleRequest");
-				testee.addController("test", controller);
-				
-			}).when("handle Request Is Called", function(){
-				testee.handleRequest(request, response, connection);
-				}).then("controller.handleRequest should be called with correct inputs", function (){
-					(controller.handleRequest.calledWith(request, response, connection)).should.be.true;
-					
-				});
+
+			request.url = "http://www.test.com:1234/v1/test";
+			unit.spy(controller, "handleRequest");
+			testee.addController("test", controller);
+
+			testee.handleRequest(request, response, connection);
+
+			(controller.handleRequest.calledWith(request, response, connection)).should.be.true;
 			done();
 		});
 		
@@ -30,18 +24,14 @@ describe("LunchLadyLand" , function (){
 			var request = new Object();
 			var response = {writeHead:function(){}, end:function(){}}; 
 			var connection = new Object(); 
-			
-			unit.given("input with unexpected route", function (){
-				request.url = "http://www.test.com:1234/v1/someRandomThing";
-				unit.spy(response, "writeHead");
-				unit.spy(response, "end");
-				
-				}).when("handle Request Is Called", function(){
-					testee.handleRequest(request, response, connection);
-				}).then("response should have 404 and end be called", function (){
-					(response.writeHead.calledWith(404)).should.be.true;
-					(response.end.calledOnce).should.be.true;
-				});
+			request.url = "http://www.test.com:1234/v1/someRandomThing";
+			unit.spy(response, "writeHead");
+			unit.spy(response, "end");
+
+			testee.handleRequest(request, response, connection);
+
+			(response.writeHead.calledWith(404)).should.be.true;
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 		
@@ -50,16 +40,12 @@ describe("LunchLadyLand" , function (){
 			var request = new Object();
 			var response = {end:function(){}}; 
 			var connection = new Object(); 
-			
-			unit.given("We have an unknown route", function (){
-				request.url = "http://www.test.com:1234/someRandomThing";
-				unit.spy(response, "end");
-				
-				}).when("handle Request Is Called", function(){
-					testee.handleRequest(request, response, connection);
-				}).then("response should have 404 and end be called", function (){
-					(response.end.calledOnce).should.be.true;
-				});
+			request.url = "http://www.test.com:1234/someRandomThing";
+			unit.spy(response, "end");
+
+			testee.handleRequest(request, response, connection);
+
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 	});
--- test/controllers/controllerFunctionsTest.js
+++ test/controllers/controllerFunctionsTest.js
@@ -2,46 +2,37 @@ var unit = require("unit.js");
 
 describe("ControllerFunctions", function(){
 	describe("isDefined", function(){
+		var testee = require("../../src/controllers/controllerFunctions");
 		it("Should return true if the field is defined", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
 			var field_object;
 			var output;
-			
-			unit.given("an object that has been defined", function(){
-				field_object = "bob 123";
-			}).when("the method is called", function(){
-				output = testee.isDefined(field_object);
-			}).then("the output should be true", function(){
-				output.should.be.true;
-			});
+			field_object = "bob 123";
+
+			output = testee.isDefined(field_object);
+
+			output.should.be.true;
 			done();
 		});
 
 		it("Should return false if the field is not defined", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
 			var field_object;
 			var output;
-			
-			unit.given("an object that has not been defined").when("the method is called", function(){
-				output = testee.isDefined(field_object);
-			}).then("the output should be false", function(){
-				output.should.be.false;
-			});
+
+			output = testee.isDefined(field_object);
+
+			output.should.be.false;
 			done();
 		});
 		
 		it("Should return true if the field is null", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
+
 			var field_object;
 			var output;
-			
-			unit.given("an object that has been defined as null", function(){
-				field_object = null;
-			}).when("the method is called", function(){
-				output = testee.isDefined(field_object);
-			}).then("the output should be true", function(){
-				output.should.be.true;
-			});
+			field_object = null;
+
+			output = testee.isDefined(field_object);
+
+			output.should.be.true;
 			done();
 		});
 
@@ -49,79 +40,65 @@ describe("ControllerFunctions", function(){
 	});
 	
 	describe("sendResponse", function(){
+		var testee = require("../../src/controllers/controllerFunctions");
 		it("Should handle having a success status and non null return object", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
 			var json;
 			var isSuccess;
 			var response = {writeHead:unit.spy(), write:unit.spy(), end:unit.spy()};
+			isSuccess = true;
+			json = "this is a json response";
 
-			unit.given("a true success flag and a response object",function(){
-				isSuccess = true;
-				json = "this is a json response";
-			}).when("the method is called",function(){
-				testee.sendResponse(response, isSuccess, json);
-			}).then("writeHead, write and end should be called on the response object", function(){
-				(response.writeHead.calledWith(200, {'Content-Type': 'application/json'})).should.be.true;
-				(response.write.calledWith("this is a json response")).should.be.true;
-				(response.end.calledOnce).should.be.true;
-			});
+			testee.sendResponse(response, isSuccess, json);
+
+			(response.writeHead.calledWith(200, {'Content-Type': 'application/json'})).should.be.true;
+			(response.write.calledWith("this is a json response")).should.be.true;
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 		
 		it("Should handle having a success status and null return object", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
 			var json;
 			var isSuccess;
 			var response = {writeHead:unit.spy(), write:unit.spy(), end:unit.spy()};
+			isSuccess = true;
+			json = null;
 
-			unit.given("a true success flag and a null response object",function(){
-				isSuccess = true;
-				json = null;
-			}).when("the method is called",function(){
-				testee.sendResponse(response, isSuccess, json);
-			}).then("writeHead, end should be called on the response object", function(){
-				(response.writeHead.calledWith(200)).should.be.true;
-				(response.write.notCalled).should.be.true;
-				(response.end.calledOnce).should.be.true;
-			});
+			testee.sendResponse(response, isSuccess, json);
+
+			(response.writeHead.calledWith(200)).should.be.true;
+			(response.write.notCalled).should.be.true;
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 		
 		it("Should handle having a non success status and non null return object", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
 			var json;
 			var isSuccess;
 			var response = {writeHead:unit.spy(), write:unit.spy(), end:unit.spy()};
+			isSuccess = false;
+			json = "this is a json response";
 
-			unit.given("a false success flag and a response object",function(){
-				isSuccess = false;
-				json = "this is a json response";
-			}).when("the method is called",function(){
-				testee.sendResponse(response, isSuccess, json);
-			}).then("writeHead, write and end should be called on the response object", function(){
-				(response.writeHead.calledWith(400, {'Content-Type': 'application/json'})).should.be.true;
-				(response.write.calledWith("this is a json response")).should.be.true;
-				(response.end.calledOnce).should.be.true;
-			});
+			testee.sendResponse(response, isSuccess, json);
+
+			(response.writeHead.calledWith(400, {'Content-Type': 'application/json'})).should.be.true;
+			(response.write.calledWith("this is a json response")).should.be.true;
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 		
 		it("Should handle having a non success status and null return object", function(done){
-			var testee = require("../../src/controllers/controllerFunctions");
+	
 			var json;
 			var isSuccess;
 			var response = {writeHead:unit.spy(), write:unit.spy(), end:unit.spy()};
+			isSuccess = false;
+			json = null;
 
-			unit.given("a false success flag and no response object",function(){
-				isSuccess = false;
-				json = null;
-			}).when("the method is called",function(){
-				testee.sendResponse(response, isSuccess, json);
-			}).then("writeHead, write and end should be called on the response object", function(){
-				(response.writeHead.calledWith(400)).should.be.true;
-				(response.write.calledWith('{ "error" : "Could not complete the request successfully." }')).should.be.true;
-				(response.end.calledOnce).should.be.true;
-			});
+			testee.sendResponse(response, isSuccess, json);
+
+			(response.writeHead.calledWith(400)).should.be.true;
+			(response.write.calledWith('{ "error" : "Could not complete the request successfully." }')).should.be.true;
+			(response.end.calledOnce).should.be.true;
 			done();
 		});
 		
@@ -129,18 +106,178 @@ describe("ControllerFunctions", function(){
 	});
 	
 	describe("buildErrorJSON", function(){
-		it("Should place a message in a json error wrapper", function(done){
 		var testee = require("../../src/controllers/controllerFunctions");
-		var message;
-		var output;
-		unit.given("a message to send", function(){
+
+		it("Should place a message in a json error wrapper", function(done){
+			var message;
+			var output;
 			message = "dancing hot dogs";
-		}).when("the method is called", function(){
+
 			output = testee.buildErrorJSON(message);
-		}).then("the output should contain the message", function(){
+
 			output.should.be.equal('{ "error" : "dancing hot dogs" }');
-		});
-		done();
+			done();
 	});
 });
+
+	describe("areValidParams", function(){
+		var testee = require("../../src/controllers/controllerFunctions");
+		
+		beforeEach(function(){
+			unit.stub(testee, "getValidParams");
+			unit.stub(testee, "validateParam");
+		});
+		
+		afterEach(function(){
+			testee.getValidParams.restore();
+			testee.validateParam.restore();
+		});
+		
+		it("Should return true if all parameters are valid", function(done){
+			
+			var query;
+			var validParamList;
+			var output;
+			query = "this is a query";
+			validParamList = ["one", "two", "three"];
+			var validParams = {one:"something", two:"somethingElse"}
+			testee.getValidParams.withArgs(query, validParamList).returns(validParams);
+			testee.validateParam.withArgs("one", "something", validParamList).returns(true);
+			testee.validateParam.withArgs("two", "somethingElse", validParamList).returns(true);
+
+			output = testee.areValidParams(query, validParamList);
+
+			output.should.be.true;
+	
+			done();
+		});
+		
+		it("Should return false if not all parameters are valid", function(done){
+			var testee = require("../../src/controllers/controllerFunctions");
+			var query;
+			var validParamList;
+			var output;
+			query = "this is a query";
+			validParamList = ["one", "two", "three"];
+			var validParams = {one:"something", two:"somethingElse"}
+			testee.getValidParams.withArgs(query, validParamList).returns(validParams);
+			testee.validateParam.withArgs("one", "something", validParamList).returns(true);
+			testee.validateParam.withArgs("two", "somethingElse", validParamList).returns(false);
+				
+			output = testee.areValidParams(query, validParamList);
+
+			output.should.be.false;
+			done();
+		});
+	});
+	
+	describe("validateParam", function (){
+		var validParams = {"param1" : "email", "param2" : "url", "param3" :"alpha", "param4" :"numeric", "param5" : "alphanumeric", "param6" : "int", "param7" : "float", "param8" : "ascii"};
+		var validator = require('validator');
+		var testee = require("../../src/controllers/controllerFunctions");
+					
+		beforeEach(function(){
+			unit.stub(validator, "isEmail");
+			unit.stub(validator, "isURL");
+			unit.stub(validator, "isAlpha");
+		});
+		
+		afterEach(function(){
+			validator.isEmail.restore();
+			validator.isURL.restore();
+			validator.isAlpha.restore();
+		});
+					
+		it("should return true if email param is validated", function(done){
+
+			var param;
+			var value;
+			var output;
+			param="param1";
+			value = "value";
+			validator.isEmail.withArgs("value").returns(true);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isEmail.called).should.be.true;
+			output.should.be.true;
+			done();
+		});
+		
+		it("should return false if email param is not validated", function(done){
+			var param;
+			var value;
+			var output;
+			param="param1";
+			value = "value";
+			validator.isEmail.withArgs("value").returns(false);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isEmail.called).should.be.true;
+			output.should.be.false;
+			done();
+		});
+		
+		it("should return true if url param is validated", function(done){
+			var param;
+			var value;
+			var output;
+			param="param2";
+			value = "value";
+			validator.isURL.withArgs("value").returns(true);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isURL.called).should.be.true;
+			output.should.be.true;
+			done();
+		});
+		
+		it("should return false if url param is not validated", function(done){
+			var param;
+			var value;
+			var output;
+			param="param2";
+			value = "value";
+			validator.isURL.withArgs("value").returns(false);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isURL.called).should.be.true;
+			output.should.be.false;
+			done();
+		});
+		
+		it("should return true if alpha param is validated", function(done){
+			var param;
+			var value;
+			var output;
+			param="param3";
+			value = "value";
+			validator.isAlpha.withArgs("value").returns(true);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isAlpha.called).should.be.true;
+			output.should.be.true;
+			done();
+		});
+		
+		it("should return false if alpha param is not validated", function(done){
+			var param;
+			var value;
+			var output;
+			param="param3";
+			value = "value";
+			validator.isAlpha.withArgs("value").returns(false);
+
+			output = testee.validateParam(param, value, validParams);
+
+			(validator.isAlpha.called).should.be.true;
+			output.should.be.false;
+			done();
+		});
+		
+	});
 });
